7d5b1c88f5c5dc0826511e62654f963c66c96c63
Mahadevan Subramanian <62354521+mahadevans2432@users.noreply.github.com>
2020-12-02 13:04:22 +0530

    Add files via upload

---
+++ postprocess.vhd
@@ -0,0 +1,21 @@
+entity postprocess is
+port( p: in bit_vector(15 downto 0);
+		carry: in bit_vector(15 downto 0);
+		sums : out bit_vector(15 downto 0);
+		cout : out bit);
+end entity postprocess;
+
+architecture postproc of postprocess is
+signal carryshift: bit_vector(15 downto 0);
+
+component XORgate15 is
+port( a_in : in bit_vector(15 downto 0);
+		b_in : in bit_vector(15 downto 0);
+		outvec : out bit_vector(15 downto 0));
+end component;
+
+begin
+cout <= carry(15);
+carryshift <= carry(14 downto 0)&'0';
+post: XORgate15 port map (carryshift,p,sums);
+end postproc; 
---
+++ prefix_unit.vhd
@@ -0,0 +1,14 @@
+entity prefix_unit is
+port(g_in1 : in bit;
+	  p_in1 : in bit;
+	  g_in2 : in bit;
+	  p_in2 : in bit;
+	  g_out : out bit;
+	  p_out : out bit);
+end entity prefix_unit;
+
+architecture prefix_comp of prefix_unit is
+begin
+g_out <= g_in1 or (p_in1 and g_in2);
+p_out <= p_in1 and p_in2;
+end prefix_comp;
