f294570f81f8e765f17f3a490e4679e4af274eaf
Drishti2708 <63410558+Drishti2708@users.noreply.github.com>
2020-12-04 14:19:30 +0530

    Update main.vhd

--- main.vhd
+++ main.vhd
@@ -1,8 +1,9 @@
+
 entity main is
 port(S0 : in bit;
 	  x : in bit_vector(15 downto 0);
 	  y : in bit_vector(15 downto 0);
-	  z : out bit_vector(15 downto 0);
+	  z : out bit_vector(15 downto 0));
 end main;
 
 architecture struct of main is
@@ -13,18 +14,21 @@ signal xor_out : bit_vector(15 downto 0);
 component mult
 port( I0: in bit;
 		I1: in bit;
-		S0: in bit;
+		S: in bit;
 		O: out bit);
 end component;
 
 begin
+random: 
 for i in 0 to 15 generate
 	nand_out(i) <= x(i) nand y(i);
 	xor_out(i) <= x(i) xor y(i);
 end generate;
+
+random2:
 	
 for i in 0 to 15 generate
-	nandxor : mult port map (nand_out(i),xor_out(i),S0,z(i))
+	nandxor : mult port map (nand_out(i),xor_out(i),S0,z(i));
 end generate;		
 	
 
